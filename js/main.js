fffc75092a3e3723ccd58efe334b6236826b62d0
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2017-01-25 18:42:58 +0000

    Search box
    Added Search Box plugin and generally cleaned up the code

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ lightbox.option({
   'wrapAround': true,
   'alwaysShowNavOnTouchDevices': true,
   'showImageNumberLabel': false,
-})
+});
+
+var options = {
+  valueNames: [ { name: 'datatitle', attr: 'data-title' } ]
+};
+var gallery = new List('gallery', options);
